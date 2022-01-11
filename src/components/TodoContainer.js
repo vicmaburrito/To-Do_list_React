c36dad4ed8c6f1240c9d1d9c76e436c63ec7e470
vicmaburrito <vmaldaraca@gmail.com>
2022-01-11 13:28:38 -0600

    Undo changes to fix functionality

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -2,8 +2,34 @@ import React from 'react';
 import TodosList from './TodosList';
 
 class TodoContainer extends React.PureComponent {
+state = {
+  todos: [
+    {
+      id: 1,
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ],
+};
+
 render() {
-  <TodosList/>
+  return (
+    <ul>
+      {this.state.todos.map((todo) => (
+        <li>{todo.title}</li>
+      ))}
+    </ul>
+  );
 }
 }
 
