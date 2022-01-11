bbb0d14e7a00e091435c21549021ef4bc0bcd187
vicmaburrito <vmaldaraca@gmail.com>
2022-01-11 12:59:58 -0600

    Remove unnecessary data in files

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -2,34 +2,8 @@ import React from 'react';
 import TodosList from './TodosList';
 
 class TodoContainer extends React.PureComponent {
-state = {
-  todos: [
-    {
-      id: 1,
-      title: 'Setup development environment',
-      completed: true,
-    },
-    {
-      id: 2,
-      title: 'Develop website and add content',
-      completed: false,
-    },
-    {
-      id: 3,
-      title: 'Deploy to live server',
-      completed: false,
-    },
-  ],
-};
-
 render() {
-  return (
-    <ul>
-      {this.state.todos.map((todo) => (
-        <li>{todo.title}</li>
-      ))}
-    </ul>
-  );
+  <TodosList/>
 }
 }
 
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -2,6 +2,26 @@ import React from 'react';
 import TodoItem from './TodoItem';
 
 class TodosList extends React.PureComponent {
+state = {
+  todos: [
+    {
+      id: 1,
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ],
+};
+
 render() {
   return (
     <ul>
