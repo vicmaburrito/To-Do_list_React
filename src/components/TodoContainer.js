7b7cecb9197f59ca1b843d41a085599185071f78
vicmaburrito <vmaldaraca@gmail.com>
2022-01-11 12:50:03 -0600

    Create Todos state and display data through components

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,13 +1,36 @@
 import React from 'react';
-class TodoContainer extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Hello from Create React App</h1>
-                <p>I am in a React Component!</p>
-            </div>
-        )
-    }
+import TodosList from './TodosList';
+
+class TodoContainer extends React.PureComponent {
+state = {
+  todos: [
+    {
+      id: 1,
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ],
 };
+
+render() {
+  return (
+    <ul>
+      {this.state.todos.map((todo) => (
+        <li>{todo.title}</li>
+      ))}
+    </ul>
+  );
+}
+}
 
 export default TodoContainer;
