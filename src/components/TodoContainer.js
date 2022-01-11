e0d98a83b61bf62ff3c51f114373df3341430980
vicmaburrito <vmaldaraca@gmail.com>
2022-01-11 15:35:05 -0600

    Add events to delete and change status

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -2,33 +2,60 @@ import React from 'react';
 import TodosList from './TodosList';
 
 class TodoContainer extends React.PureComponent {
-state = {
-  todos: [
-    {
-      id: 1,
-      title: 'Setup development environment',
-      completed: true,
-    },
-    {
-      id: 2,
-      title: 'Develop website and add content',
-      completed: false,
-    },
-    {
-      id: 3,
-      title: 'Deploy to live server',
-      completed: false,
-    },
-  ],
-};
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Setup development environment',
+        completed: true,
+      },
+      {
+        id: 2,
+        title: 'Develop website and add content',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Deploy to live server',
+        completed: false,
+      },
+    ],
+  };
+  handleChange = id => {
+    this.setState(prevState => {
+      return {
+        todos: prevState.todos.map(todo => {
+          if (todo.id === id) {
+            return {
+              ...todo,
+              completed: !todo.completed,
+            }
+          }
+          return todo
+        }),
+      }
+    });
+  };
+
+  delTodo = id => {
+    this.setState({
+      todos: [
+        ...this.state.todos.filter(todo => {
+          return todo.id !== id;
+        })
+      ]
+    });
+  };
 
 render() {
   return (
-    <ul>
-      {this.state.todos.map((todo) => (
-        <li>{todo.title}</li>
-      ))}
-    </ul>
+      <section>
+      <TodosList 
+      todos={this.state.todos} 
+      handleChangeProps={this.handleChange}
+      deleteTodoProps={this.delTodo}
+      />
+      </section>
   );
 }
 }
