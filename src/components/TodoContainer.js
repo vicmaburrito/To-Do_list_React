1816082b9fee232aaf34eac9eb1c301a514c05ce
vicmaburrito <vmaldaraca@gmail.com>
2022-01-11 23:22:24 -0600

    Fix linter erros

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,59 +1,67 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import TodosList from './TodosList';
-import InputTodo from './InputTodo'
+import InputTodo from './InputTodo';
 import Header from './Header';
-import { v4 as uuidv4 } from "uuid";
-
 
 class TodoContainer extends React.PureComponent {
-  state = {
-    todos: [],
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
+  handleChange = (id) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      }),
+    }));
   };
-  handleChange = id => {
-    this.setState(prevState => {
-      return {
-        todos: prevState.todos.map(todo => {
-          if (todo.id === id) {
-            return {
-              ...todo,
-              completed: !todo.completed,
-            }
-          }
-          return todo
-        }),
-      }
+
+  delTodo = (id) => {
+    this.setState({
+      todos: [
+        ...this.state.todos.filter((todo) => todo.id !== id),
+      ],
     });
   };
 
-  delTodo = id => {
-    this.setState({
-      todos: [
-        ...this.state.todos.filter(todo => {
-          return todo.id !== id;
-        })
-      ]
-    });
-  };
-  addTodoItem = title => {
+  addTodoItem = (title) => {
     const newTodo = {
       id: uuidv4(),
-      title: title,
-      completed: false
+      title,
+      completed: false,
     };
     this.setState({
-      todos: [...this.state.todos, newTodo]
+      todos: [...this.state.todos, newTodo],
     });
   };
+
   setUpdate = (updatedTitle, id) => {
     this.setState({
-      todos: this.state.todos.map(todo => {
+      todos: this.state.todos.map((todo) => {
         if (todo.id === id) {
-          todo.title = updatedTitle
+          todo.title = updatedTitle;
         }
-        return todo
+        return todo;
       }),
-    })
+    });
   }
+
   render() {
     return (
       <div className="container">
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,36 +1,42 @@
 import React from 'react';
-import styles from "./TodoItem.module.css"
+import styles from './TodoItem.module.css';
+
 class TodoItem extends React.Component {
-  state = {
-    editing: false,
+  constructor() {
+    super();
+    this.state = {
+      editing: false,
+    };
   }
+
   handleEditing = () => {
     this.setState({
       editing: true,
-    })
+    });
   }
 
-  handleUpdatedDone = event => {
-    if (event.key === "Enter") {
-      this.setState({ editing: false })
+  handleUpdatedDone = (event) => {
+    if (event.key === 'Enter') {
+      this.setState({ editing: false });
     }
   }
+
   render() {
     const completedStyle = {
-      fontStyle: "italic",
-      color: "#595959",
+      fontStyle: 'italic',
+      color: '#595959',
       opacity: 0.4,
-      textDecoration: "line-through",
+      textDecoration: 'line-through',
     };
-    const { completed, id, title } = this.props.todo
+    const { completed, id, title } = this.props.todo;
 
-    let viewMode = {}
-    let editMode = {}
+    const viewMode = {};
+    const editMode = {};
 
     if (this.state.editing) {
-      viewMode.display = "none"
+      viewMode.display = 'none';
     } else {
-      editMode.display = "none"
+      editMode.display = 'none';
     }
     return (
       <li className={styles.item}>
@@ -41,7 +47,7 @@ class TodoItem extends React.Component {
             checked={completed}
             onChange={() => this.props.handleChangeProps(id)}
           />
-          <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+          <button type="button" onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
           <span style={completed ? completedStyle : null}>{title}</span>
         </div>
         <input
