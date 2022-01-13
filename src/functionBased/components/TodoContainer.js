93afaf239af724a2e6d8094912dcabb48a629fe2
vicmaburrito <vmaldaraca@gmail.com>
2022-01-13 14:26:03 -0600

    Set app to router

--- src/functionBased/components/TodoContainer.js
+++ src/functionBased/components/TodoContainer.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { Route, Switch } from 'react-router-dom';
 import TodosList from './TodosList';
 import InputTodo from './InputTodo';
 import Header from './Header';
